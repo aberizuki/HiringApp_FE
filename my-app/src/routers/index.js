638a467c6347b854dc6fc29f57510a0c315c3b74
permana017 <ahdipermana07@gmail.com>
2023-01-31 12:22:56 +0700

    update: setup react and tailwind

---
+++ my-app/src/routers/index.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes , Route, Navigate, redirect } from 'react-router-dom';
+
+
+
+const App = () =>{
+    return(
+        <Router>
+            <Routes>
+            {/* <Route path="/" element={<Home/>} /> */}
+            </Routes>
+        </Router>
+    )
+}
