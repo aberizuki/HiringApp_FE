b7ad5482a51c91aca24b22f433b2db93a6e14767
permana017 <ahdipermana07@gmail.com>
2023-01-31 19:11:38 +0700

    update: home

---
+++ my-app/src/Section/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+
+function Navbar() {
+    return(
+        <div>
+            <div className="container">
+
+            </div>
+        </div>
+    )
+}
+
+
+export default Navbar
---
+++ my-app/src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "src/Section/Navbar";
+
+
+
+function Home() {
+    return(
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
+
+
+export default Home
